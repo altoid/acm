64b8cfbf8cd9b6607345be06abfdc72bd5810e44
Doug Tomm <dtomm@radiumone.com>
2018-06-01 23:04:18 -0700

    run length encoding for program strings

--- 2011/addmul/src/main/scala/AddMul.scala
+++ 2011/addmul/src/main/scala/AddMul.scala
@@ -2,6 +2,14 @@ package addmul
 
 // file:///Users/dtomm/Downloads/2011WorldFinalProblemSet.pdf
 
+object ProgramOrdering extends Ordering[String] {
+  def compare(a:String, b:String) = {
+    val cmp = a.length compare b.length
+    if (cmp == 0) a compare b
+    else cmp
+  }
+}
+
 class AddMul(val addend: Int, val multiplicand: Int, val output_lowend: Int, val output_highend: Int) {
   require(addend > 0)
   require(multiplicand > 0)
@@ -9,7 +17,7 @@ class AddMul(val addend: Int, val multiplicand: Int, val output_lowend: Int, val
   require(output_highend > 0)
   require(output_lowend <= output_highend)
 
-  def helper(partial_program: List[Char], partial_result: Int, operation: Char, level: Int): List[String] = {
+  def generateHelper(partial_program: List[Char], partial_result: Int, operation: Char, level: Int): List[String] = {
     println(("    " * level) + partial_program.mkString + ", " + partial_result + ", " + operation)
 
     val result_of_operation = operation match {
@@ -18,8 +26,8 @@ class AddMul(val addend: Int, val multiplicand: Int, val output_lowend: Int, val
     }
 
     if (result_of_operation < output_lowend) {
-      val result_by_mult = helper(partial_program :+ operation, result_of_operation, 'M', level + 1)
-      val result_by_add = helper(partial_program :+ operation, result_of_operation, 'A', level + 1)
+      val result_by_mult = generateHelper(partial_program :+ operation, result_of_operation, 'M', level + 1)
+      val result_by_add = generateHelper(partial_program :+ operation, result_of_operation, 'A', level + 1)
 
       List(result_by_mult, result_by_add).flatten
     }
@@ -32,12 +40,28 @@ class AddMul(val addend: Int, val multiplicand: Int, val output_lowend: Int, val
     }
   }
 
+  def encodeList(prog: List[Char]): List[String] = {
+    prog match {
+      case Nil => List()
+      case h :: t => {
+        val (front, back) = prog.span(p => p == h)
+        val l = front.length
+        val s = f"$l%d$h%c"
+        s :: encodeList(back)
+      }
+    }
+  }
+
+  def encode(program: String): String = {
+    // run-length encode a string of As and Ms.
+    encodeList(program.toList).mkString(" ")
+  }
+
   def generatePrograms(input: Int): List[String] = {
-    val result_by_mult = helper(List(), input, 'M', 1)
-    val result_by_add = helper(List(), input, 'A', 1)
+    val result_by_mult = generateHelper(List(), input, 'M', 1)
+    val result_by_add = generateHelper(List(), input, 'A', 1)
 
     val result = List(result_by_mult, result_by_add).flatten
-//    println(result)
     result
   }
 
@@ -50,6 +74,40 @@ class AddMul(val addend: Int, val multiplicand: Int, val output_lowend: Int, val
     }
 
     generatePrograms(input).sortWith(mySortFunc).head
+  }
+
+  def runHelper(partial_result: Int, program: List[Char]): Int = {
+    program match {
+      case 'M' :: t => runHelper(partial_result * multiplicand, t)
+      case 'A' :: t => runHelper(partial_result + addend, t)
+      case h :: t => throw new IllegalArgumentException(s"wtf is $h?")
+      case Nil => partial_result
+    }
+  }
+
+  def runProgram(input: Int, program: String): Int = {
+    val plist = program.toList
+    runHelper(input, plist)
+  }
+
+  def solve(p: Int, q: Int) = {
+    if (output_lowend <= p && q <= output_highend) "empty"
+    else {
+      var programs = generatePrograms(q)
+
+      // println(programs)
+
+      for (k <- p until q) {
+        programs = programs.filter { x =>
+          val result = runProgram(k, x)
+          result >= output_lowend && result <= output_highend
+        }
+      }
+      if (programs.isEmpty) "impossible"
+      else {
+        programs.sorted(ProgramOrdering).head
+      }
+    }
   }
 }
 
--- 2011/addmul/src/test/scala/AddMulTest.scala
+++ 2011/addmul/src/test/scala/AddMulTest.scala
@@ -4,7 +4,7 @@ import org.scalatest.FunSuite
 
 class AddMulTest extends FunSuite {
 
-  test("test1") {
+  ignore("test1") {
     val addmul = new AddMul(1, 3, 22, 33)
 
     val input = 3
@@ -13,14 +13,87 @@ class AddMulTest extends FunSuite {
     println(s"$input:  $best")
   }
 
-  test("sorting") {
+  ignore("running") {
+    val addmul = new AddMul(1, 3, 22, 33)
+
+    val input = 3
+
+    var result = addmul.runProgram(input, "MM")
+    assert(result == 27)
+    result = addmul.runProgram(input, "AA")
+    assert(result == 5)
+    result = addmul.runProgram(input, "AAMM")
+    assert(result == 45)
+  }
+
+  ignore("sorting") {
     def mySortFunc(a: String, b: String): Boolean = {
-      // sort by length then lexicographically
+      // sort by length then lexicographically in reverse
       if (a.length < b.length) true
       else if (b.length < a.length) false
-      else a < b
+      else a > b
     }
+
     val t = List("mmm", "aaa", "mm", "am", "ma", "aa")
-    println(t.sortWith(mySortFunc))
+    assert(List("mm", "ma", "am", "aa", "mmm", "aaa") == t.sortWith(mySortFunc))
+    assert(List("mm", "ma", "am", "aa", "mmm", "aaa") == t.sorted(ProgramOrdering))
+  }
+
+  test("encode") {
+    val addmul = new AddMul(1, 2, 10, 20)
+    val uncompiled = List("AMM", "MAM", "AAAM", "AMAM", "MAAM", "AAAAM", "MAAAM", "AAAAAM", "AAAAAAM")
+    val test = uncompiled.map(x => addmul.encode(x))
+    assert(List("1A 2M", "1M 1A 1M", "3A 1M", "1A 1M 1A 1M", "1M 2A 1M", "4A 1M", "1M 3A 1M", "5A 1M", "6A 1M") == test)
+  }
+
+  ignore("run programs on all inputs") {
+    val addmul = new AddMul(1, 2, 10, 20)
+
+    val p = 2
+    val q = 3
+    var programs = addmul.generatePrograms(q)
+
+    println(programs)
+
+//    // gimme all the programs that work for 2
+//    val x = programs.filter { p =>
+//      val result = addmul.runProgram(2, p)
+//      result >= addmul.output_lowend && result <= addmul.output_highend
+//    }
+//    println(x)
+
+    for (k <- p until q) {
+      programs = programs.filter { x =>
+        val result = addmul.runProgram(k, x)
+        result >= addmul.output_lowend && result <= addmul.output_highend
+      }
+    }
+    println(">>>>>>>>>>>>>")
+    println(programs.sorted(ProgramOrdering))
+    println("<<<<<<<<<<<<<")
+  }
+
+  ignore("should be empty program") {
+    val addmul = new AddMul(5, 3, 2, 3)
+    val solution = addmul.solve(2, 3)
+    println(solution)
+  }
+
+  ignore("impossible") {
+    val addmul = new AddMul(3, 2, 4, 5)
+    val solution = addmul.solve(2, 3)
+    println(solution)
+  }
+
+  ignore("example 1") {
+    val addmul = new AddMul(1, 2, 10, 20)
+    val solution = addmul.solve(2, 3)
+    println(solution)
+  }
+
+  ignore("example 2") {
+    val addmul = new AddMul(1, 3, 22, 33)
+    val solution = addmul.solve(2, 3)
+    println(solution)
   }
 }
